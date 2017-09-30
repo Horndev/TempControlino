7f29510428d506c76d3485dab4fb5a43f4d5a2db
Steven Horn <horn@horndev.com>
2017-09-30 18:20:50 -0400

    Remove compile folders from repo (should not be there)

--- TempControlino/Debug/EventButtonPressed.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\pc1\AppData\Local\Temp\VMBuilds\TempControlino\nano_atmega328\Debug\EventButtonPressed.cpp.o: \
- C:\Users\pc1\AppData\Local\Temp\VMBuilds\TempControlino\nano_atmega328\Debug\EventButtonPressed.cpp \
- C:\Users\pc1\AppData\Local\Temp\VMBuilds\TempControlino\nano_atmega328\Debug\EventButtonPressed.h
--- TempControlino/Debug/EventClicked.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\pc1\AppData\Local\Temp\VMBuilds\TempControlino\nano_atmega328\Debug\EventClicked.cpp.o: \
- C:\Users\pc1\AppData\Local\Temp\VMBuilds\TempControlino\nano_atmega328\Debug\EventClicked.cpp \
- C:\Users\pc1\AppData\Local\Temp\VMBuilds\TempControlino\nano_atmega328\Debug\EventClicked.h
